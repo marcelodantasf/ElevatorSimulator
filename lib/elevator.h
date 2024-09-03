8908ab391dd742f5a073efb6ea9df7f529daa0a9
marcelodantasf <mdantasfilho2005@gmail.com>
2024-09-03 17:33:07 -0300

    feat: declared new functions

--- lib/elevator.h
+++ lib/elevator.h
@@ -3,18 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "building.h"
 #include "util.h"
 #include "floor.h"
 
-enum Way
+typedef enum 
 {
     DOWN,
     UP
-};
+} way;
 
-typedef enum Way way;
+//typedef enum Way way;
 
-typedef struct
+/*typedef struct
 {
     int noButtons;
     floor* selected;
@@ -24,18 +25,31 @@ typedef struct
 {
    way direction;
    bool isPressed;
-} externalButton;
+} externalButton; */
 
 
 typedef struct
 {
    floor* current;
-   internalButton ib;
-   externalButton eb;
+   //internalButton ib;
+   //externalButton eb;
+   floor* selected;
 } elevator;
+
+typedef struct
+{
+    floor* destiny;
+    way dir;
+} route;
 
 elevator standByMode(elevator* e, floor* ground);
 
-void toggleButton(elevator* e);
+//void toggleButton(elevator* e);
+
+route addToRoute(floor* f, elevator* e, way d);
+
+elevator whichCloser(floor* f, building b);
+
+void move(elevator* e);
 
 #endif
--- lib/util.h
+++ lib/util.h
@@ -1,5 +1,8 @@
 #ifndef UTIL_H
 #define UTIL_H
+#include "floor.h"
+#include "elevator.h"
+#include "building.h"
 
 typedef char* String;
 
@@ -9,5 +12,7 @@ typedef struct
     struct node* prev;
     struct node* next;
 } node;
+
+route trimString(String str, building b);
 
 #endif
